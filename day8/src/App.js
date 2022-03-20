1572fe5169ffa7482f75cfb0faa06035959af67c
divya53200 <divya5320.doodleblue@gmail.com>
2022-03-20 10:22:40 +0530

    'divya'

---
+++ day8/src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Register from './Register'
+import Submit from './Submit'
+import Input from './Input.js'
+
+
+class App extends Component{
+  render(){
+
+  return(
+    <div>
+    
+    <Register/>
+    <Submit/>
+    <Input/>
+    </div>
+      )
+}
+}
+export default App;
+
